e22ffaf7c2b6c3d43e33108b09341d96347d1dc5
Valeria da Silva Cavania <1485655@ifms.edu.br>
2025-11-28 14:31:58 -0400

    adicionando o readme

---
+++ Readme.mk
@@ -0,0 +1,15 @@
+# Olá, Bem vindo ao meu projeto de portfólio.
+
+## Ferramentas utilizadas:
+
+* HTML
+
+* CSS
+
+* Flex-box
+
+## Feito por:
+
+### Valéria
+
+### Linkedin: https://www.linkedin.com/in/valeria-cavania/
